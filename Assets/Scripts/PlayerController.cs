7f8e4c0dbd1cd6f6db1bda426426efe89746ae04
rafaelem2 <rafaelem2@al.insper.edu.br>
2023-04-12 15:02:20 -0300

    quase andando

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed = 5f;
+    Vector2 moveInput;
+
+    private bool _isMoving = false;
+    public bool IsMoving { get
+        {
+            return _isMoving;
+        } 
+        private set
+            {
+                _isMoving = value;
+                animator.SetBool("isMoving", value);
+            }
+    }
+
+    private bool isRunning = false;
+    public bool _isRunning { get
+        {
+            return _isRunning;
+        } 
+        private set
+            {
+                _isRunning = value;
+                animator.SetBool("isRunning", value);
+            }
+    }
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        IsMoving = moveInput != Vector2.zero;
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            isRunning = true;
+        } else if(context.canceled)
+        {
+            isRunning = false;
+        }
+    }
+}
