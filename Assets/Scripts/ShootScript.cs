ad367fbcc52a40a13d4367d96cab0656d4f5538c
rafaelem2 <rafaelem2@al.insper.edu.br>
2023-04-15 19:22:27 -0300

    triste

---
+++ Assets/Scripts/ShootScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+    public Transform Gun;
+
+    Vector2 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = mousePos - (Vector2)Gun.position;
+        Debug.Log(mousePos);
+        FaceMouse();
+    }
+
+    void FaceMouse()
+    {
+        Gun.transform.right = direction;
+    }
+
+    
+
+}
