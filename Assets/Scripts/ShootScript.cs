6ac7b4472ced062ce48b21964cdf790eeb314a9c
rafaelem2 <rafaelem2@al.insper.edu.br>
2023-04-15 20:57:25 -0300

    inimigo

--- Assets/Scripts/ShootScript.cs
+++ Assets/Scripts/ShootScript.cs
@@ -1,32 +1,35 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShootScript : MonoBehaviour
 {
     public Transform Gun;
+    public Transform Player;
 
     Vector2 direction;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        direction = mousePos - (Vector2)Gun.position;
-        Debug.Log(mousePos);
+
+        if (Player.localScale.x < 0) {
+            direction = (Vector2)Gun.position - mousePos;
+        } else {
+            direction = mousePos - (Vector2)Gun.position;
+        }
+
         FaceMouse();
     }
 
     void FaceMouse()
     {
-        Gun.transform.right = direction;
+        // Check if the player is facing in the same direction as the mouse
+        if (Player.localRotation.eulerAngles.y == 0 && direction.x > 0 || 
+            Player.localRotation.eulerAngles.y == 180 && direction.x < 0)
+        {
+            Gun.transform.right = direction;
+        }
     }
-
-    
-
 }
