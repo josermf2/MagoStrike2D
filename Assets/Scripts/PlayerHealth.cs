201a43ce838d87016259db21fce63af54478a705
josermf2 <josermf2@al.insper.edu.br>
2023-05-01 19:42:06 -0300

    bug fixes

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -56,6 +56,8 @@ public class PlayerHealth : MonoBehaviour
 
         if (SceneManager.GetActiveScene().buildIndex > 2){
             pickupQuantity = PlayerPrefs.GetInt("Cash");
+
+            PlayerPrefs.DeleteAll();
         }
     }
 
